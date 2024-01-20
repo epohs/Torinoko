da9daccdd991c5da6e490640295701c0774e012c
Jason Shope <blurryphoto@gmail.com>
2024-01-20 11:21:00 -0500

    cleanup and reorganize app initialization

--- app/__init__.py
+++ app/__init__.py
@@ -11,25 +11,20 @@ def create_app( config_class=Config ):
   app = Flask(__name__)
   app.config.from_object(config_class)
 
-  # Initialize Flask extensions here
-  db.init_app(app)
-
-  from app.models.note import Note
-  
   
 
   with app.app_context():
 
+    # Initialize the database
+    db.init_app(app)
+
+    # Create tables if they don't exist	
     db.create_all()
     db.session.commit()	
 
-    #test_note = Note( content='hey this is a test note' )
-    #db.session.add(test_note)
-    #db.session.commit()
 
 
-
-  # Register blueprints here
+  # Register blueprints and routes
   from app.main import bp as main_bp
   from app.main import routes
   
