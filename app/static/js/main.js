2698ec4752158cab1ba1e033632f5625bd167529
Jason Shope <blurryphoto@gmail.com>
2024-01-22 09:30:38 -0500

    first basic character counter

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,4 +1,15 @@
 
+
+
+const passphrase_field = document.getElementById('passphrase');
+const counter_el = document.querySelector(".field-wrap.field-passphrase .passphrase-char-count .cur-chars");
+
+
+
+
+
+
+
 // Wait until HTML has been parsed
 document.addEventListener('DOMContentLoaded', () => {
   
@@ -6,5 +17,19 @@ document.addEventListener('DOMContentLoaded', () => {
   document.body.classList.remove('no-js');
 
 
+  // Update the counter on every keystroke
+  passphrase_field.addEventListener('keyup', update_passphrase_counter);
+
   
 }); // ( DOMContentLoaded )
+
+
+
+
+
+function update_passphrase_counter() {
+
+  const characterCount = passphrase_field.value.length;
+  counter_el.textContent = characterCount;
+
+}
